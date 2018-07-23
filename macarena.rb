8e5e9dc9e005d975aee7e1e6afac071bbec5b186
vaalcat <val.cat456@icloud.com>
2018-07-23 20:48:06 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -12,7 +12,7 @@ puts "Step 1: Right arm out in front of you, palm facing down."
 puts "Left arm out in front of you, palm facing down"
 
 # Step 3: FIXME
-puts "Step 3: "Turn your right palm up"
+puts "Step 3: Turn your right palm up"
 
 # Step 4: FIXME
 puts "Step 4: Turn your left palm up."
@@ -21,7 +21,7 @@ puts "Step 4: Turn your left palm up."
 puts "Step 5: Bring your right hand up and around to the back of your head."
 
 # Step 6: FIXME
-puts "Step 6: Left arm out in front of you, palm facing down."
+puts "Step 6: Left arm out in front of you, palm facing down"
 
 # Step 7: FIXME
 puts "Step 7: Place your left hand on your left shoulder."
